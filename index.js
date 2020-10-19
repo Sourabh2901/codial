972e9e75043369227fe4995896ffd5e1b316d809
Sourabh Rai <sourabhrai99990@gmail.com>
2020-10-19 17:11:21 +0530

    add_a controller and router for home

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const app     = express();
 const port    = 8000;
 
 //Use Express Router file--------By default require('./routes') fetches index.js
-app.use('/',require('./routes'));
+app.use(require('./routes'));
 
 app.listen(port,function(err){
     if(err){
-        // console.log("Error While Connecting to DB!!");
         console.log(`Error in running the server: ${err}`);
     }
     console.log(`Server is running on port : ${port}`);
